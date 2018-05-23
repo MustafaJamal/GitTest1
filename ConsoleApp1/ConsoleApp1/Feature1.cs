db46e6e38052a89dcd9d50e05c43ae87cd6f35cb
Mustafa Jamal <mustafa.jamal@edevtech.com>
2018-05-23 15:05:41 +0500

    Add methode

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,5 +8,13 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+
+            return sum;
+        }
     }
 }
