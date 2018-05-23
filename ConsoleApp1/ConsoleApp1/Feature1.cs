e73062192aea221049b4d0324eced56cbb8f7c75
Mustafa Jamal <mustafa.jamal@edevtech.com>
2018-05-23 15:09:17 +0500

    Refactored code

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,13 +8,9 @@ namespace ConsoleApp1
 {
     class Feature1
     {
-        public int Add()
+        public int Add(int x1, int x2)
         {
-            int x1 = 1;
-            int x2 = 2;
-            int sum = x1 + x2;
-
-            return sum;
+            return x1 + x2;
         }
     }
 }
