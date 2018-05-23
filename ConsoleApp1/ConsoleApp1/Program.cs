968dc0aab2ec0e891a2ad61b4fc2602547378528
Mustafa Jamal <mustafa.jamal@edevtech.com>
2018-05-23 14:33:44 +0500

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Code was added in GitHub
+            // Code was edited in GitHub
         }
     }
 }
